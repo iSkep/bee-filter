84f6515fa5fc8465a81b917126dcbd37582ea433
Skep <madskep@gmail.com>
2023-04-27 19:19:01 +0300

    Add table load from json

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,14 +3,17 @@ import { flsModules } from './modules.js';
 import * as flsFunctions from './functions.js';
 import * as flsForms from './forms/forms.js';
 import { rangeItems } from './forms/range.js';
+import { postData, getResource } from '../files/services/services.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     resetBtnInit();
+    getResource('http://localhost:3001/numbers?_embed=products').then((data) => createTable(data));
+
     document.addEventListener('click', documentActions);
 
-    // Actions (click event delegation)
     function documentActions(e) {
         const targetElement = e.target;
+
         if (window.innerWidth > 768 && isMobile.any()) {
             if (targetElement.classList.contains('menu__arrow')) {
                 targetElement.closest('.menu__item').classList.toggle('_hover');
@@ -58,5 +61,83 @@ document.addEventListener('DOMContentLoaded', function () {
                 });
             });
         }
+    }
+
+    function createTable(data) {
+        data.forEach(({ number, date, products }) => {
+            const parentSelector = document.querySelector('.spoilers');
+            const element = document.createElement('div');
+            element.classList.add('spoilers__item');
+            const tableRows = products
+                .map((product) => {
+                    let clothType;
+
+                    switch (product.clothId) {
+                        case '1':
+                            clothType = 'jacket';
+                            break;
+                        case '2':
+                            clothType = 'watch';
+                            break;
+                        case '3':
+                            clothType = 'pants';
+                            break;
+                        case '4':
+                            clothType = 'robe';
+                            break;
+                        case '5':
+                            clothType = 'blouse';
+                            break;
+                        case '6':
+                            clothType = 'shoes';
+                            break;
+                        default:
+                            clothType = '';
+                    }
+
+                    return `
+                    <tr>
+                        <td>${product.type}</td>
+                        <td>${product.hairiness}</td>
+                        <td class="spoilers__image">
+                            <svg>
+                                <use xlink:href="../../img/icons/sprite.svg#svg-${clothType}"></use>
+                            </svg>
+                        </td>
+                        <td class="spoilers__price">${product.price}</td>
+                    </tr>
+                `;
+                })
+                .join('');
+
+            element.innerHTML = `
+            <button class="spoilers__button" type="button" data-spoiler>
+                <span class="spoilers__number">№${number}</span>
+                <span class="spoilers__date">${date}</span>
+            </button>
+            <div class="spoilers__body" hidden>
+                <table class="spoilers__table">
+                    <colgroup>
+                        <col class="spoilers__col-1" />
+                        <col class="spoilers__col-2" />
+                        <col class="spoilers__col-3" />
+                        <col class="spoilers__col-4" />
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th>Вид пчел</th>
+                            <th>Волосатость брюшка пчелы</th>
+                            <th>Одежда</th>
+                            <th>Цена</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${tableRows}
+                    </tbody>
+                </table>
+            </div>`;
+
+            parentSelector.append(element);
+        });
     }
 });
